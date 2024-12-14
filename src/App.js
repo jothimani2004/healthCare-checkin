6e530a919f8523e60dfa0725604f46fbce0cb923
jothimani2004 <jothimani88531@gmail.com>
2024-12-14 21:09:05 +0530

    adding route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './frontend/authentication/login';
 import Register from './frontend/authentication/register';
 import ResetPassword from './frontend/authentication/resetpassword';
